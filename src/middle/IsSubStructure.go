044f8fbf4e728ee0c4de88ce399b5a52af41ef1b
admin <admin@MacBook-Pro.local>
2020-06-22 00:18:03 +0800

    添加树的子结构代码中的注释

--- src/middle/IsSubStructure.go
+++ src/middle/IsSubStructure.go
@@ -2,6 +2,42 @@ package main
 
 import "common"
 
+/*
+	@title:树的子结构
+	@description:输入两棵二叉树A和B，判断B是不是A的子结构。(约定空树不是任意一个树的子结构)
+	B是A的子结构， 即 A中有出现和B相同的结构和节点值。
+
+	@example:
+	例如:
+	给定的树 A:
+	     3
+	    / \
+	   4   5
+	  / \
+	 1   2
+	给定的树 B：
+	   4 
+	  /
+	 1
+	返回 true，因为 B 与 A 的一个子树拥有相同的结构和节点值。
+
+	示例 1：
+	输入：A = [1,2,3], B = [3,1]
+	输出：false
+	示例 2：
+	输入：A = [3,4,5,1,2], B = [4,1]
+	输出：true
+
+	限制：
+	0 <= 节点个数 <= 10000
+
+	来源：力扣（LeetCode）
+	链接：https://leetcode-cn.com/problems/shu-de-zi-jie-gou-lcof
+ */
+
+/*
+	@version:Go 1.13.12
+*/
 func main() {
 	treeNode1 := common.TreeNode{
 		Val: 1,
@@ -37,7 +73,9 @@ func main() {
 	print("判断结果：",isSubStructure(&treeNode1, &testTreeNode1), " （true表示B是A的子结构，false表示B不是A的子结构）")
 }
 
-
+/*
+	将B树与A树，A树左子树，A树右子树进行比较，递归进行即可。
+ */
 func isSubStructure(A *common.TreeNode, B *common.TreeNode) bool {
 	if A == nil || B == nil {
 		return false
@@ -45,6 +83,9 @@ func isSubStructure(A *common.TreeNode, B *common.TreeNode) bool {
 	return subStructure(A, B) || isSubStructure(A.Left, B) || isSubStructure(A.Right, B)
 }
 
+/*
+	判断A树是否包含B树，包含条件是A树的根节点与B树是相同的。
+ */
 func subStructure(A *common.TreeNode, B *common.TreeNode) bool {
 
 	if B == nil {
